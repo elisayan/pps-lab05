d045e447d762cc1742b3e64fe4801e47a449763b
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-30 09:50:24 +0200

    implemented Vector2D

--- src/main/scala/it/unibo/pps/ex/Vector2D.scala
+++ src/main/scala/it/unibo/pps/ex/Vector2D.scala
@@ -6,6 +6,7 @@ import scala.math.sqrt // Needed for magnitude calculation
 // Structure: x-component and y-component
 trait Vector2D:
   def x: Double
+
   def y: Double
 
   // Vector addition: (x1, y1) + (x2, y2) = (x1+x2, y1+y2)
@@ -25,21 +26,34 @@ trait Vector2D:
 
 object Vector2D:
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = new Vector2DImpl(x, y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
   val i: Vector2D = apply(1.0, 0.0) // Unit vector along x-axis
   val j: Vector2D = apply(0.0, 1.0) // Unit vector along y-axis
 
-
 /** Hints:
  *   - Implement Vector2D with a Vector2DImpl class.
  *   - Initially, use a regular `class`. Check that equality (==) and
- *      toString do not behave as you might expect for a value object representing a vector.
+ *     toString do not behave as you might expect for a value object representing a vector.
  *   - Modify the implementation to use a `case class Vector2DImpl` instead.
  *   - Observe how equality (==) and toString now work correctly out-of-the-box.
  */
+
+case class Vector2DImpl(override val x: Double, override val y: Double) extends Vector2D {
+
+  override def +(other: Vector2D): Vector2D = Vector2DImpl(this.x + other.x, this.y + other.y)
+
+  override def -(other: Vector2D): Vector2D = Vector2DImpl(this.x - other.x, this.y - other.y)
+
+  override def *(scalar: Double): Vector2D = Vector2DImpl(scalar * this.x, scalar * this.y)
+
+  override def dot(other: Vector2D): Double = (this.x * other.x) + (this.y * other.y)
+
+  override def magnitude: Double = sqrt(x * x + y * y)
+}
+
 @main def checkVectors(): Unit =
   val v1 = Vector2D(3.0, 4.0)
   val v2 = Vector2D(-1.0, 2.0)
