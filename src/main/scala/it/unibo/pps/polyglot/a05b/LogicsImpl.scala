3cebac94fae0a1cb2b029d47cde0ead32bb3d7ae
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-30 11:40:48 +0200

    implemented task 2

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,22 @@
 package it.unibo.pps.polyglot.a05b
 
+import it.unibo.pps.polyglot.Pair
 import it.unibo.pps.polyglot.a05b.Logics
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
 
-  override def isOver: Boolean = false
+  private var tickCount = 0
+  private val random = scala.util.Random()
+  private val initial = Pair[Int, Int](random.nextInt(size - 2) + 1, random.nextInt(size - 2) + 1)
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def tick(): Unit = tickCount = tickCount + 1
+
+  override def isOver: Boolean = initial.getY - tickCount < 0 || initial.getY + tickCount >= size || initial.getX - tickCount < 0 || initial.getX + tickCount >= size
+
+  override def hasElement(x: Int, y: Int): Boolean =
+    (x == initial.getX && ((y - initial.getY).abs <= tickCount)) ||
+      (y == initial.getY && ((y - initial.getX).abs <= tickCount)) ||
+      (x - y == initial.getX - initial.getY && (x - initial.getX).abs <= tickCount) ||
+      (x + y == initial.getX - initial.getY && (x - initial.getX).abs <= tickCount)
+
